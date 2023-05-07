afe61064692518434d35d8498682da1c0d243db8
Vincent Coralde <coraldevincent1@gmail.com>
2023-05-07 15:00:33 +0800

    change

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
  // Import the functions you need from the SDKs you need
  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-app.js";
- import { GoogleAuthProvider, getAuth, signInWithPopup, OAuthProvider } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-auth.js";
+//  import { GoogleAuthProvider, getAuth, signInWithPopup, OAuthProvider } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-auth.js";
  import { getDatabase, set, ref, update } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-database.js";
  import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword,} from "https://www.gstatic.com/firebasejs/9.21.0/firebase-auth.js";
  // TODO: Add SDKs for Firebase products that you want to use
@@ -22,8 +22,8 @@
  const app = initializeApp(firebaseConfig);
  const database = getDatabase(app);
  const auth = getAuth();
- const googleProvider = new GoogleAuthProvider(app);
- const microsoftProvider = new OAuthProvider('microsoft.com');
+//  const googleProvider = new GoogleAuthProvider(app);
+//  const microsoftProvider = new OAuthProvider('microsoft.com');
 
  signUp.addEventListener('click',()=>{
    
@@ -53,47 +53,47 @@
  });
 
 //MICROSOFT
-   microsoft.addEventListener('click',(e)=>{
-    signInWithPopup(auth, microsoftProvider)
-    .then((result) => {
-      // User is signed in.
-      // IdP data available in result.additionalUserInfo.profile.
+  //  microsoft.addEventListener('click',(e)=>{
+  //   signInWithPopup(auth, microsoftProvider)
+  //   .then((result) => {
+  //     // User is signed in.
+  //     // IdP data available in result.additionalUserInfo.profile.
   
-      // Get the OAuth access token and ID Token
-      const credential = OAuthProvider.credentialFromResult(result);
-      const accessToken = credential.accessToken;
-      const idToken = credential.idToken;
-    })
-    .catch((error) => {
-      // Handle error.
-    });
-   });
+  //     // Get the OAuth access token and ID Token
+  //     const credential = OAuthProvider.credentialFromResult(result);
+  //     const accessToken = credential.accessToken;
+  //     const idToken = credential.idToken;
+  //   })
+  //   .catch((error) => {
+  //     // Handle error.
+  //   });
+  //  });
 
-  // GOOGLE
-  google.addEventListener('click',(e)=>{
-    signInWithPopup(auth, googleProvider)
-    .then((result) => {
-      // This gives you a Google Access Token. You can use it to access the Google API.
-      const credential = GoogleAuthProvider.credentialFromResult(result);
-      const token = credential.accessToken;
-      // The signed-in user info.
-      const user = result.user;
-      alert(user.displayName);
-      // IdP data available using getAdditionalUserInfo(result)
-      // ...
-    }).catch((error) => {
-      // Handle Errors here.
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      // The email of the user's account used.
-      const email = error.customData.email;
-      // The AuthCredential type that was used.
-      const credential = GoogleAuthProvider.credentialFromError(error);
-      // ...
-      alert(errorMessage);
-    });
+  // // GOOGLE
+  // google.addEventListener('click',(e)=>{
+  //   signInWithPopup(auth, googleProvider)
+  //   .then((result) => {
+  //     // This gives you a Google Access Token. You can use it to access the Google API.
+  //     const credential = GoogleAuthProvider.credentialFromResult(result);
+  //     const token = credential.accessToken;
+  //     // The signed-in user info.
+  //     const user = result.user;
+  //     alert(user.displayName);
+  //     // IdP data available using getAdditionalUserInfo(result)
+  //     // ...
+  //   }).catch((error) => {
+  //     // Handle Errors here.
+  //     const errorCode = error.code;
+  //     const errorMessage = error.message;
+  //     // The email of the user's account used.
+  //     const email = error.customData.email;
+  //     // The AuthCredential type that was used.
+  //     const credential = GoogleAuthProvider.credentialFromError(error);
+  //     // ...
+  //     alert(errorMessage);
+  //   });
   
-  });
+  // });
   login.addEventListener('click', ()=>{
     var email = document.getElementById('Email').value;
     var password = document.getElementById('Password').value;
