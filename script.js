dd03efa96347ef0eb7cde53e83997b06bbee5785
Vincent Coralde <coraldevincent1@gmail.com>
2023-05-07 14:14:14 +0800

    change

--- script.js
+++ script.js
@@ -53,45 +53,47 @@
  });
 
 //MICROSOFT
-   microsoft.addEventListener('click',(e)=>{
-    signInWithPopup(auth, microsoftProvider)
-    .then((result) => {
-      // User is signed in.
-      // IdP data available in result.additionalUserInfo.profile.
+  //  microsoft.addEventListener('click',(e)=>{
+  //   signInWithPopup(auth, microsoftProvider)
+  //   .then((result) => {
+  //     // User is signed in.
+  //     // IdP data available in result.additionalUserInfo.profile.
   
-      // Get the OAuth access token and ID Token
-      const credential = OAuthProvider.credentialFromResult(result);
-      const accessToken = credential.accessToken;
-      const idToken = credential.idToken;
-    })
-    .catch((error) => {
-      // Handle error.
-    });
-   });
+  //     // Get the OAuth access token and ID Token
+  //     const credential = OAuthProvider.credentialFromResult(result);
+  //     const accessToken = credential.accessToken;
+  //     const idToken = credential.idToken;
+  //   })
+  //   .catch((error) => {
+  //     // Handle error.
+  //   });
+  //  });
 
   // GOOGLE
   google.addEventListener('click',(e)=>{
-    signInWithPopup(auth, googleProvider, email, password)
-            .then((userCredential) => {
-    // Signed in 
-                const user = userCredential.user;
- 
-                const dt = new Date();
-                update(ref(database, 'users/' + user.uid),{
-                    last_login: dt,
-                })
-                alert('User Logged In!');
-    // ...
-        })
-        .catch((error) => {
-            const errorMessage = error.message;
- 
-            alert(errorMessage);
-        });
- 
+    signInWithPopup(auth, googleProvider)
+    .then((result) => {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+      // The signed-in user info.
+      const user = result.user;
+      alert(user.displayName);
+      // IdP data available using getAdditionalUserInfo(result)
+      // ...
+    }).catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.customData.email;
+      // The AuthCredential type that was used.
+      const credential = GoogleAuthProvider.credentialFromError(error);
+      // ...
+      alert(errorMessage);
     });
-
-
+  
+  });
   login.addEventListener('click', ()=>{
     var email = document.getElementById('Email').value;
     var password = document.getElementById('Password').value;
